5298e8479a5b16bb0c6719423e506d9badeef6ea
user <user@DESKTOP-4P2GO7K>
2022-12-15 16:54:35 +0100

    first commit

---
+++ HelloAjax2/WebContent/js/jsfunctions.js
@@ -0,0 +1,20 @@
+function showHello(str) {
+	var request = new XMLHttpRequest();//NON IE
+
+
+	request.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(request.responseText);
+			document.getElementById("ServerResponse").innerHTML = request.response.Text;
+
+		}
+	};
+
+
+
+	//il punto interrogativo è per separare la risorsa dalla lista dei parametri
+	request.open("GET", "saluto?user=" + str, true);//setto la richiesta verso il server
+	request.send();//invio
+
+
+}
